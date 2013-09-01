fe2b9bf40f9f802e5f2eae3903213427cc597879
pedroargento <peter.argent@gmail.com>
2013-09-02 00:50:14 +1000

    quiz

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,4 @@
 class Link < ActiveRecord::Base
   belongs_to :week
+  default_scope order: 'links.created_at ASC'
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,4 @@
 class Quiz < ActiveRecord::Base
   belongs_to :week
+  default_scope order: 'quizzes.created_at ASC'
 end

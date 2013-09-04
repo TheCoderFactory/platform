8e70f1fb9f6b21fa1268431f0f1aca12f0df5e66
pedroargento <peter.argent@gmail.com>
2013-09-04 14:32:55 +1000

    calendar and events

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,6 @@
 class CalendarController < ApplicationController
   def index
+    @future_events = Event.published.future(Date.today)
+    @events = Event.published
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ActiveRecord::Base
 	scope :published, -> { where(publish: true) }
+	default_scope order: 'events.date ASC'
   	
   	def self.future(date)
-	   where("date < ?", date)
+	   where("date > ?", date)
 	end
 end

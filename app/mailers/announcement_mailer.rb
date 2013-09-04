4bfb7d68db6eaf9be4b71fff5864b46e18240d51
pedroargento <peter.argent@gmail.com>
2013-09-04 13:07:26 +1000

    final announcement mailer

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -3,7 +3,7 @@ class AnnouncementMailer < ActionMailer::Base
 
   def announcement_email(announcement)
   	@announcement = announcement
-  	@students = User.with_role :admin
+  	@students = User.with_role :student
 	@student_emails = @students.map { |u| u.email }
 	mail(:to => @student_emails, :subject => "The Coder Factory Announcement: " + announcement.title)
   end

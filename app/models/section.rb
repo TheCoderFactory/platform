b87a7b005f69b26d701f4caf0bcdd87b122a91aa
pedroargento <peter.argent@gmail.com>
2013-09-06 00:21:28 +1000

    default scope

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ActiveRecord::Base
   belongs_to :inclass
-  default_scope { order(:priority) }
+  default_scope order: 'sections.priority ASC'
   extend FriendlyId
   friendly_id :title, use: :slugged
 end

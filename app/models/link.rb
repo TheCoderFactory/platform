0fdad4470114bc82fe7152d6a481d9321b286b70
pedroargento <peter.argent@gmail.com>
2013-09-09 00:11:03 +1000

    tags

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,17 @@
 class Link < ActiveRecord::Base
   belongs_to :week
+  acts_as_taggable
+  acts_as_taggable_on :topics
+
   default_scope order: 'links.created_at ASC'
   scope :quick, -> { where(quick: true) }
+
+  # def self.by_week(week)
+  #   where("week == ?", week)
+  # end
+
+  # def self.by_topic(topic)
+  # 	where("topic == ?", topic)
+  # end
+
 end

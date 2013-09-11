61ad6f462dc3dea4f79670ec8a0ef81dac393fdf
pedroargento <peter.argent@gmail.com>
2013-09-11 11:55:42 +1000

    updated site

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -1,2 +1,7 @@
 module WeeksHelper
+	def favicon(link)
+		scheme = URI(link.url).scheme
+		host = URI(link.url).host
+		return "http://g.etfv.co/" + scheme + "://" + host
+	end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,9 @@
 class Link < ActiveRecord::Base
   belongs_to :week
 
-  default_scope order: 'links.created_at ASC'
+  default_scope order: 'links.created_at DESC'
   scope :quick, -> { where(quick: true) }
 
+  
 
 end
